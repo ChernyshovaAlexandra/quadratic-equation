17661ff1ee3bcc23a5da746a9cb93253c1c99aea
ChernyshovaAlexandra <alexandrachrystal@gmail.com>
2018-02-23 16:50:26 +0300

    implementation of function 2

--- src/index.js
+++ src/index.js
@@ -1,28 +1,34 @@
 module.exports = function solveEquation(equation) {
- var D;
-  var cur = [];
-  var quadric = equation.split(' '); //это строка
-  var param = [];
+    var D;
+    var cur = [];
+    var quadric = equation.split(' ');
+    var param = [];
 
-  param.push((quadric.slice(0, quadric.indexOf('*')).join('')), 
-             (quadric.slice(quadric.indexOf('x^2')+1, quadric.lastIndexOf('*')).join("")),
-             (quadric.slice(quadric.indexOf('x')+1, quadric[-1])).join(''));
-for(var i = 0; i < param.length; i++){
-  param[i]=== Number(param[i]);
-}
-  D = Math.pow(param[1], 2) - (4 * param[0] * param[2]);
- if(D > 0){
-    cur[0] = (-param[1] + Math.sqrt(D)) / (2 *param[0]); 
-    cur[1] = (-param[1] - Math.sqrt(D)) / (2* param[0]);
-  return cur.sort();
+    param.push(
+        +(quadric.slice(0, quadric.indexOf('*')).join('')),
+        +(quadric.slice(quadric.indexOf('x^2') + 1, quadric.lastIndexOf('*')).join("")),
+        +(quadric.slice(quadric.indexOf('x') + 1, quadric[-1])).join('')
+    );
 
-  } else if(D < 0){
-    D = D * (-1);
-    cur[0] = (-param[1] + Math.round(Math.sqrt(D))) / (2 * param[0]);
-    cur[1] = (-param[1] - Math.round(Math.sqrt(D))) / (2 * param[0]);
-    return Math.round(cur[0]), Math.round(cur[1]);
-  } else if(D === 0){
-      cur = (-param[1] + Math.sqrt(D)) / (2 * param[0]);
-      return cur.sort()
-  }
+    D = Math.pow(param[1], 2) - (4 * param[0] * param[2]);
+    if (D != 0) {
+        D = D < 0 ? D * -1 : D;
+        cur[0] = Math.round((-param[1] + Math.sqrt(D)) / (2 * param[0]));
+        cur[1] = Math.round((-param[1] - Math.sqrt(D)) / (2 * param[0]));
+        return cur.sort(compare);
+
+    } else {
+        cur = (-param[1] + Math.sqrt(D)) / (2 * param[0]);
+        return cur;
+    }
+};
+
+function compare(a, b) {
+    if (a < b) {
+        return -1;
+    }
+    if (a > b) {
+        return 1;
+    }
+    return 0;
 }
